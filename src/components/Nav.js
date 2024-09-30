05de1678672b465f5d72533c1faf791c861bad5d
Calvinbullock <calvinnbullock@gmail.com>
2024-09-30 11:51:23 -0600

    navbar component set up

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img src="" alt="logo"/>
+            <input type="" name="search" value="search" placeholder="search"/>
+            <a href="DEAD">login</a>
+        </div>
+    )
+}
+
+export default Nav
