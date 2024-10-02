66e3b38deaeac6329c7ad9215140c99c38fa878c
Calvinbullock <calvinnbullock@gmail.com>
2024-10-02 17:17:25 -0600

    nav update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,8 +5,8 @@ function Nav() {
     return (
         <div className="nav">
             <img src="" alt="logo"/>
-            <input type="" name="search" value="search" placeholder="search"/>
-            <a href="DEAD">login</a>
+            <input id="nav-search" type="" name="search" value="search" placeholder="search"/>
+            <button id="signIn-button" type="">Sign In</button>
         </div>
     )
 }
