8a870e6d789923bae5252fa8801a45e31ab709a2
Calvinbullock <calvinnbullock@gmail.com>
2024-10-02 17:18:37 -0600

    added a note component to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import Nav from "./Nav";
+import NoteCard from "./NoteCard";
+
+const props = {
+    title: "shop", 
+    date: "Nov 2, 2024",
+    content: "lorm pop yop",
+};
 
 function Home() {
     return (
         <div className="home">
             <Nav/>
+            <NoteCard {...props}/>
+
+
         </div>
     )
 }
