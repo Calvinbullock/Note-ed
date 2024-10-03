b044c1499ef7eef6ce3b066b8add57b12bc6a055
Calvinbullock <calvinnbullock@gmail.com>
2024-10-03 10:51:53 -0600

    dark mode button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,7 @@ function Nav() {
             <img src="" alt="logo"/>
             <input id="nav-search" type="" name="search" value="search" placeholder="search"/>
             <button id="signIn-button" type="">Sign In</button>
+            <button id="dark-mode-switch" type="">Dark Mode</button>
         </div>
     )
 }
