ef2ae77971c615c6188cb159bf88bea4e134ee5d
Calvinbullock <calvinnbullock@gmail.com>
2024-10-05 11:43:14 -0600

    name updates

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+
+import React from "react";
+import { getNotesFromLocalStorage } from "../utils/utils";
+
+import Nav from "./Nav";
+import NoteCard from "./NoteCard";
+import NoteEditor from "./NoteEditor";
+
+// TODO: need to add an event listener for a new note added then re render the
+//      note list
+// TODO: need an idea for each note
+
+// NOTE: layout like this???
+//     ___________________
+//     |______nav________|
+//     |      | note note|
+//     | edit | note note|
+//     |      | note note|
+//     -------------------
+
+const props = {
+    title: "shop", 
+    date: "Nov 2, 2024",
+    text: "lorm pop yop",
+};
+
+function Home() {
+    let notesList = getNotesFromLocalStorage()
+
+    return (
+        <div className="home">
+            <Nav/>
+            <NoteEditor {...props}/>
+            
+            {notesList != null && notesList.map((element, index) => (
+                <NoteCard key={index} {...element} />
+            ))}
+        </div>
+    )
+}
+
+export default Home
