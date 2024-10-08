b80f5dc76854a5725da47cb53bc11a84104d8c9b
Calvinbullock <calvinnbullock@gmail.com>
2024-10-08 11:29:10 -0600

    update again

--- config/firebase.js
+++ config/firebase.js
@@ -7,9 +7,6 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-thishfidshafkjdsaklsda
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
