39661d6344148ad48090e5f8326f7c9a4cd32da8
Calvinbullock <calvinnbullock@gmail.com>
2024-10-08 11:20:50 -0600

    database start

--- src/dataBase/data.js
+++ src/dataBase/data.js
@@ -1,4 +1,19 @@
 import { collection, doc, setDoc } from "firebase/firestore"; 
 import { db } from "../config/firebase"
 
-const q = query(citiesRef, where('country', 'in', ['USA', 'Japan']));
+// Add a second document with a generated ID.
+import { addDoc, collection } from "firebase/firestore"; 
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  });
+
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
