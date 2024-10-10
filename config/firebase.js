bf846eb10370b49896d535da5a22fb740c9365ae
Calvinbullock <calvinnbullock@gmail.com>
2024-10-10 13:24:25 -0600

    firebase update

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { firebaseConfig } from "env,js";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID,
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -12,3 +22,7 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db }
+
+
+// NOTE: rest api
+// NOTE: send email to bro keers -- STOP BY??
