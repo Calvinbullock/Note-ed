e22fcbfaec204fd49f74d3fac1059ba21a2ecd81
Calvinbullock <calvinnbullock@gmail.com>
2024-10-11 13:01:41 -0600

    fetch works with unsecured key
    unsecured key not pushed

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,11 +1,33 @@
 
-import React from "react";
 import { deleteNoteFromLocal } from "../utils/utils";
+import React from 'react';
+import axios from 'axios';
 
 const NoteCard = (props) => {
 
+    const fetchNotes = () => {
+        const fetchData = async () => {
+            try {
+                console.log("response")
+                response = await axios.get(
+                    'https://firestore.googleapis.com/v1/projects/noted-eeafd/databases/(default)/documents/Notes', {
+                        headers: {
+                            'Authorization': `Bearer ----`
+                        }
+                    });
+                console.log("Done")
+                //setResponse(response.data);
+                console.log(response)
+            } catch (error) {
+                console.log("error")
+                console.error(error);
+            }
+        }
+        let response = fetchData();
+    }
+
     const deleteNote = () => {
-        deleteNoteFromLocal(props.id)
+        deleteNoteFromLocal(props.id);
     }
 
     return (
@@ -14,8 +36,9 @@ const NoteCard = (props) => {
             <p>{props.date}</p>
             <p>{props.text}</p>
             <button onClick={deleteNote} type="deleteNote">DELETE</button>
+            <button onClick={fetchNotes} type="deleteNote">Fetch</button>
         </div>
-    )
+    );
 }
 
-export default NoteCard
+export default NoteCard;
