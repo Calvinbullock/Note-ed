4d6aa061409a8f0a8d4cb9b31c41f4674ffcf949
Calvinbullock <calvinnbullock@gmail.com>
2024-10-14 11:34:15 -0600

    homePage update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,6 +6,29 @@ import Nav from "./Nav";
 import NoteCard from "./NoteCard";
 import NoteEditor from "./NoteEditor";
 
+const props = {
+    title: "shop", 
+    date: "Nov 2, 2024",
+    text: "lorm pop yop",
+};
+
+export default function HomePage() {
+    let notesList = getNotesFromLocalStorage();
+
+    return (
+        <div className="home">
+            <Nav/>
+            <NoteEditor {...props}/>
+            
+            {notesList != null && notesList.map((element, index) => (
+                <NoteCard key={index} {...element} />
+            ))}
+        </div>
+    );
+}
+
+
+
 // TODO: need to add an event listener for a new note added then re render the
 //      note list
 // TODO: need an idea for each note
@@ -17,26 +40,3 @@ import NoteEditor from "./NoteEditor";
 //     | edit | note note|
 //     |      | note note|
 //     -------------------
-
-const props = {
-    title: "shop", 
-    date: "Nov 2, 2024",
-    text: "lorm pop yop",
-};
-
-function Home() {
-    let notesList = getNotesFromLocalStorage()
-
-    return (
-        <div className="home">
-            <Nav/>
-            <NoteEditor {...props}/>
-            
-            {notesList != null && notesList.map((element, index) => (
-                <NoteCard key={index} {...element} />
-            ))}
-        </div>
-    )
-}
-
-export default Home
