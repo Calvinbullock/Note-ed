beaabe4d826925a1667a29919684f87e4c64b604
Calvinbullock <calvinnbullock@gmail.com>
2024-10-25 10:40:20 -0600

    sign out from nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import { useNavigate, Link } from 'react-router-dom';
+import { auth } from "./../config/firebase";
 
 export default function Nav() {
     
@@ -14,11 +15,22 @@ export default function Nav() {
         console.log("TODO: in Nav.jsx");
     }
 
+    const handleSearch = () => {
+        console.log("TODO: in Nav.jsx");
+    }
+
     return (
         <div className="nav">
             <Link to="/"><img src="" alt="logo" /></Link>
-            <input id="nav-search" type="" name="search" value="search" placeholder="search"/>
-            <button onClick={handleSignIn} id="signIn-button" type="">Sign In</button>
+            <input onChange={handleSearch} id="nav-search" type="" name="search" value="search" placeholder="search"/>
+
+            {(auth.currentUser == null) ? (
+                <button onClick={handleSignIn} id="signIn-button" type="">Sign In</button>
+            ) : (
+                <button onClick={() => auth.signOut() } id="signIn-button" type="">Sign Out</button>
+            )}
+
+
             <button onClick={handleDarkModeChange} id="dark-mode-switch" type="">Dark Mode</button>
         </div>
     );
